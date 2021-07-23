48b2fb97dc186724484793ecc4aefa0aab594b14
Larry Garfield <larry@garfieldtech.com>
2021-07-23 13:24:46 -0500

    Add a compose function that supports nesting.

---
+++ src/composition.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crell\fp;
+
+function compose(callable|iterable ...$fns): callable
+{
+    return static function (mixed $arg) use ($fns): mixed  {
+        foreach ($fns as $fn) {
+            $arg = is_iterable($fn) ? compose(...$fn)($arg) : $fn($arg);
+        }
+        return $arg;
+    };
+}
