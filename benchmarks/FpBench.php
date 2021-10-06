1fd96c4352861f9ba89dee211ef57e38d88e90b7
Larry Garfield <larry@garfieldtech.com>
2021-10-06 13:53:19 +0200

    Add PHPBench for testing.

--- benchmarks/FpBench.php
+++ benchmarks/FpBench.php
@@ -10,6 +10,9 @@ use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\OutputTimeUnit;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 use PhpBench\Benchmark\Metadata\Annotations\Warmup;
+use function Crell\fp\pipe_compose;
+use function Crell\fp\pipe_iterate;
+use function Crell\fp\pipe_reduce;
 
 /**
  * @Revs(10)
@@ -19,9 +22,31 @@ use PhpBench\Benchmark\Metadata\Annotations\Warmup;
  * @AfterMethods({"tearDown"})
  * @OutputTimeUnit("milliseconds", precision=3)
  */
-class AttributeUtilsBenchmarks
+class FpBenchmarks
 {
-    public function setUp(): void {}
+    protected array $funcs;
+
+    public function setUp(): void
+    {
+        $func = static fn($x) => $x;
+        $this->funcs = array_fill(0, 10, $func);
+    }
 
     public function tearDown(): void {}
+
+    public function benchPipeCompose(): void
+    {
+        pipe_compose(5, ...$this->funcs);
+    }
+
+    public function benchPipeIterate(): void
+    {
+        pipe_iterate(5, ...$this->funcs);
+    }
+
+    public function benchPipeReduce(): void
+    {
+        pipe_reduce(5, ...$this->funcs);
+    }
+
 }
